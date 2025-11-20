2e6e685371a0a45f2c1a2c20388c1c5e5446564c
Fardin <fardinfarrokhzad@gmail.com>
2025-11-20 13:04:21 +0330

    add custom labels to BarChart for displaying values on bars

--- src/pages/Dashboard/components/BarChart/BarChart.jsx
+++ src/pages/Dashboard/components/BarChart/BarChart.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
-import { BarChart as RBarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer, CartesianGrid, Cell } from 'recharts';
-import { numberToPersian } from '/src/utils/formatters';
+import {
+  BarChart as RBarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  ResponsiveContainer,
+  CartesianGrid,
+  Cell,
+  LabelList,
+} from 'recharts';
 
 function BarChart({ monthlyIncome, monthlyExpense }) {
   const data = [
@@ -10,17 +18,46 @@ function BarChart({ monthlyIncome, monthlyExpense }) {
 
   const COLORS = ['#3ebd93', '#ef4e4e'];
 
+  const CustomLabel = props => {
+    const { x, y, width, value, index } = props;
+    return (
+      <text
+        x={x + width / 2}
+        y={y - 8}
+        fill={COLORS[index]}
+        textAnchor='middle'
+        fontSize={11}
+        fontWeight='bold'
+      >
+        {value}
+      </text>
+    );
+  };
+
   return (
     <ResponsiveContainer width='100%' height={260}>
-      <RBarChart data={data} margin={{ top: 10, right: 20, left: 5, bottom: 10 }}>
+      <RBarChart
+        data={data}
+        margin={{ top: 30, right: 20, left: 20, bottom: 10 }}
+      >
         <CartesianGrid strokeDasharray='3 3' opacity={0.3} />
-        <XAxis dataKey='name' tick={{ fontSize: 12 }} axisLine={false} tickLine={false} />
-        <YAxis tick={{ fontSize: 12 }} axisLine={false} tickLine={false} width={40} tickFormatter={value => numberToPersian(value)} />
-        <Tooltip contentStyle={{ fontSize: 12 }} formatter={value => numberToPersian(value)} />
-        <Bar dataKey='value' radius={[6, 6, 0, 0]}>
+        <XAxis
+          dataKey='name'
+          tick={{ fontSize: 10, fontWeight: 600 }}
+          axisLine={false}
+          tickLine={false}
+        />
+        <YAxis
+          tick={{ fontSize: 11 }}
+          axisLine={false}
+          tickLine={false}
+          width={50}
+        />
+        <Bar dataKey='value' radius={[8, 8, 0, 0]}>
           {data.map((entry, index) => (
             <Cell key={`cell-${index}`} fill={COLORS[index]} />
           ))}
+          <LabelList content={<CustomLabel />} />
         </Bar>
       </RBarChart>
     </ResponsiveContainer>
